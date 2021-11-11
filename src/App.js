7fdc060f621752b4c0b1d9f3fc7c1ae71b595c88
Settarov Ali <settarov.ali@gmail.com>
2021-11-12 01:18:26 +0200

    Add MainNavigation and Layout components

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import AllQuotes from './pages/AllQuotes';
 import QuoteDetail from './pages/QuoteDetail';
 import NewQuote from './pages/NewQuote';
 import Comments from './components/comments/Comments';
+import Layout from './components/layout/Layout';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="quotes" />} />
-      <Route path="quotes" element={<AllQuotes />} />
-      <Route path="quotes/:quoteId" element={<QuoteDetail />}>
-        <Route path="comments" element={<Comments />} />
-      </Route>
-      <Route path="new-quote" element={<NewQuote />} />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Navigate to="quotes" />} />
+        <Route path="quotes" element={<AllQuotes />} />
+        <Route path="quotes/:quoteId" element={<QuoteDetail />}>
+          <Route path="comments" element={<Comments />} />
+        </Route>
+        <Route path="new-quote" element={<NewQuote />} />
+      </Routes>
+    </Layout>
   );
 }
 
